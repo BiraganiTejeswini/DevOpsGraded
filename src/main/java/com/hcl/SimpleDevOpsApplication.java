6003b257ef165e3467f932c373b25b34db37c2bd
BiraganiTejeswini <tejaswiyadav107@gmail.com>
2021-06-04 18:44:45 +0530

    second save

--- src/main/java/com/hcl/SimpleDevOpsApplication.java
+++ src/main/java/com/hcl/SimpleDevOpsApplication.java
@@ -8,8 +8,9 @@ public class SimpleDevOpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleDevOpsApplication.class, args);
+		System.out.println(display());
 	}
-	public String display() {
+	public static String display() {
 		return "DevOps Graded.........";
 	}
 
